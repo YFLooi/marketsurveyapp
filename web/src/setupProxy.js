a70b9d9c0c617c86e9f88cc5735874808bec1392
YFLooi <yihfoo@gmail.com>
2020-04-05 18:53:35 +0800

    raised PORT to 6000

--- web/src/setupProxy.js
+++ web/src/setupProxy.js
@@ -3,5 +3,5 @@ const { createProxyMiddleware } = require('http-proxy-middleware')
 console.log(`PORT given to client: ${process.env.PORT}`)
 
 module.exports = app => {
-  app.use('/api/*', createProxyMiddleware({ target: `http://localhost:80/`, changeOrigin: true }))
+  app.use('/api/*', createProxyMiddleware({ target: `http://localhost:6000/`, changeOrigin: true }))
 };
