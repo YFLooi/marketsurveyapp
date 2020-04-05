4dd8fa4d623f23534a4f039b0fcc4e3b8717ac20
YFLooi <yihfoo@gmail.com>
2020-04-05 16:44:55 +0800

    added console logs to check PORT

--- web/src/setupProxy.js
+++ web/src/setupProxy.js
@@ -1,6 +1,6 @@
 const { createProxyMiddleware } = require('http-proxy-middleware')
 
-console.log(`process.env.port is: ${process.env.PORT}`)
+console.log(`PORT given to client: ${process.env.PORT}`)
 
 module.exports = app => {
   app.use('/api/*', createProxyMiddleware({ target: `http://localhost:80/`, changeOrigin: true }))
