97483f415eb76c5f302333763f1737567fda2542
YFLooi <yihfoo@gmail.com>
2020-04-05 16:32:00 +0800

    removed PORT from .env, see if it fixes PORT to 80

--- api/server.js
+++ api/server.js
@@ -14,10 +14,6 @@ const db = require('./db/queries.js')
 // Use bodyParser to parse JSON
 app.use(bodyParser.json())
 
-//Static file declaration, which is the location of the React app
-//Used in deployment by React app to access index.js
-app.use(express.static(path.join(__dirname, 'build'))); 
-
 //tell a route making a GET request on the root (/) URL to head to the HomePage
 app.get("/api/", (request, response) => {
     if (error) {
@@ -32,12 +28,19 @@ app.get("/api/testFunction", db.testFunction)
 app.get("/api/testGet", db.testGet)
 app.get("/api/testHerokuPg", db.testHerokuPg)
 
-//Put this last among all routes. Otherwise, it will return HTML to all fetch requests and trip up CORS. They interrupt each other
-// For any request that doesn't match, this sends the index.html file from the client. This is used for all of our React code.
-//Eliminates need to set redirect in package.json at start script with concurrently
-app.get('*', (req, res) => {  
-    res.sendFile(path.join(__dirname+'/build/index.html'));
-})
+//Only used in production, since I do not build before running in development
+if(process.env.NODE_ENV == "production"){
+    //Static file declaration, which is the location of the React app
+    //Used in deployment by React app to access index.js
+    app.use(express.static(path.join(__dirname, 'build'))); 
+
+    //Put this last among all routes. Otherwise, it will return HTML to all fetch requests and trip up CORS. They interrupt each other
+    // For any request that doesn't match, this sends the index.html file from the client. This is used for all of our React code.
+    //Eliminates need to set redirect in package.json at start script with concurrently
+    app.get('*', (req, res) => {  
+        res.sendFile(path.join(__dirname+'/build/index.html'));
+    })
+}
 
 /*set the app to listen on the port you set*/
 app.listen(port, () => {
