65342197891ad4d9d0b0f69b22b293d0bc44b9bd
YFLooi <yihfoo@gmail.com>
2020-04-05 15:01:05 +0800

    placed .docker proxy settings together with each Dockerfile.

--- web/src/setupProxy.js
+++ web/src/setupProxy.js
@@ -3,3 +3,4 @@ const { createProxyMiddleware } = require('http-proxy-middleware')
 module.exports = app => {
   app.use('/api/*', createProxyMiddleware({ target: "http://localhost:6000/", changeOrigin: true }))
 };
+
