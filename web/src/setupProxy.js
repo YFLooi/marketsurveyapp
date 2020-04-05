ee2466567d67709d50ce49260090033db4afca99
YFLooi <yihfoo@gmail.com>
2020-04-05 16:42:18 +0800

    added back PORT into api's .env to stop Heroku error, but force port to 80 by setting app.listen to port 80. set ARG port=90 inside web's Dockerfile to see if both api and web get the same PORT set

--- web/src/setupProxy.js
+++ web/src/setupProxy.js
@@ -3,10 +3,13 @@ const { createProxyMiddleware } = require('http-proxy-middleware')
 console.log(`process.env.port is: ${process.env.PORT}`)
 
 module.exports = app => {
+  app.use('/api/*', createProxyMiddleware({ target: `http://localhost:80/`, changeOrigin: true }))
+};
+
+/**
   if (process.env.PORT === 80){
-    app.use('/api/*', createProxyMiddleware({ target: `http://localhost:80/`, changeOrigin: true }))
+    
   } else {
     app.use('/api/*', createProxyMiddleware({ target: `http://localhost:${process.env.PORT}/`, changeOrigin: true }))
   }
-};
-
+ */
