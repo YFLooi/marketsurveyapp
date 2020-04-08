fe6e1c1cc4fe2fa4f93c670fd284b97da35723c5
YFLooi <yihfoo@gmail.com>
2020-04-08 16:41:28 +0800

    added comment

--- web/src/setupProxy.js
+++ web/src/setupProxy.js
@@ -3,5 +3,6 @@ const { createProxyMiddleware } = require('http-proxy-middleware')
 console.log(`PORT given to client: ${process.env.PORT}`)
 
 module.exports = app => {
-  app.use('/api/*', createProxyMiddleware({ target: `http://localhost:6000/`, changeOrigin: true }))
+  //Sends proxy request to http://localhost:6000/api/*
+  app.use('/api', createProxyMiddleware({ target: `http://localhost:6000/`, changeOrigin: true }))
 };
