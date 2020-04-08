f4ccbe46e2f312b867a89ea11bfb89c05229feab
YFLooi <yihfoo@gmail.com>
2020-04-08 18:29:53 +0800

    merged api and web into one folder. React app now in web/client

--- web/client/src/setupProxy.js
+++ web/client/src/setupProxy.js
@@ -3,6 +3,6 @@ const { createProxyMiddleware } = require('http-proxy-middleware')
 console.log(`PORT given to client: ${process.env.PORT}`)
 
 module.exports = app => {
-  //Sends proxy request to http://localhost:6000/api/*
-  app.use('/api', createProxyMiddleware({ target: `http://localhost:6000/`, changeOrigin: true }))
+  //Sends proxy request to http://localhost:6000/*
+  app.use('/server/*', createProxyMiddleware({ target: `http://localhost:6000`, changeOrigin: true }))
 };
