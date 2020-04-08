390f78b0b5e3017e8a6e0efea3325efdb3d2c038
YFLooi <yihfoo@gmail.com>
2020-04-08 20:29:52 +0800

    removed errors

--- web/src/setupProxy.js
+++
@@ -1,8 +0,0 @@
-const { createProxyMiddleware } = require('http-proxy-middleware')
-
-console.log(`PORT given to client: ${process.env.PORT}`)
-
-module.exports = app => {
-  //Sends proxy request to http://localhost:6000/api/*
-  app.use('/api', createProxyMiddleware({ target: `http://localhost:6000/`, changeOrigin: true }))
-};
