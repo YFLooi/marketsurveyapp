5aaefb919f0d87ef2e9699f19030331b654c54bb
YFLooi <yihfoo@gmail.com>
2020-05-24 14:44:42 +0800

    redid sliders to work with Material UI's sliders. New values submitted onMouseUp, so delay expected on pc

--- web/client/src/components/SurveyPage/RateScale.js
+++ web/client/src/components/SurveyPage/RateScale.js
@@ -1,12 +1,32 @@
 import React, { useState, useEffect } from "react";
 import { makeStyles } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
+import Slider from '@material-ui/core/Slider';
 
 const useStyles = makeStyles((theme) => ({
     root: {
       width: 300,
     },
-    
+    margin: {
+      height: theme.spacing(3),
+    },
+    thumb: { //For the dot that is dragged
+        height: 24,
+        width: 24,
+        backgroundColor: '#fff',
+        border: '2px solid currentColor',
+        marginTop: -8,
+        marginLeft: -12,
+        '&:focus, &:hover, &$active': {
+          boxShadow: 'inherit',
+        },
+    },
+    mark: { //For the lines denoting the scale values
+        backgroundColor: 'gray',
+        height: 10,
+        width: 3,
+        marginTop: -3,
+    }
 }));
 
 //Labels points on scale and value returned when on said point
@@ -14,123 +34,104 @@ const marks = [
     {
         value: 1,
         label: '1',
-        index: "index1"
       },
       {
         value: 2,
         label: '2',
-        index: "index2"
       },
       {
         value: 3,
         label: '3',
-        index: "index3"
       },
       {
         value: 4,
         label: '4',
-        index: "index4"
       },
       {
         value: 5,
         label: '5',
-        index: "index5"
       }
 ];
 
-//Returns newly-selected value at slider position
-function valuetext(value, index) {
-    console.log(`index supplied: ${index}`)
+//Sets value in pop-up balloon
+function valuetext(value) {
     return `${value}`;
 }  
 
 //props available: data, questionResponse
 function RateScale(props) {
+    const classes = useStyles(); 
+
     const questionData = props.data;
     const responseKeys = Object.keys(questionData.responseText)
     const responseValues = Object.values(questionData.responseText)
 
-    let [ sourceName, setSourceName] = useState("");
-    let [ rating, setRating ] = useState(param => {
+    let newValue = ""; //Using hook state causes error "setX is not a function"
+    let [ recordedValues, setRecordedValues ] = useState(param => {
         let newObj = {};
         for (let i=0; i<responseKeys.length; i++){
-            newObj[responseKeys[i]] = "3"; //Sets default slider rating on page load
+            newObj[responseKeys[i]] = 3; //Sets default slider value on page load
         }
 
         return newObj;
     });
-    
-    const classes = useStyles(); 
-    
-    useEffect(() => {  
-        
-        
-        /*
-        const questionData = props.data;
-        const responseKeys = Object.keys(questionData.responseText);
-        const responseValues = Object.values(questionData.responseText);
-        const dragNDropData = Array(responseKeys.length).fill().map(
-            function(item, i) {
-                return {
-                    id: responseKeys[i],
-                    content: responseValues[i]
-                }
-        })
-        
-        //Generates initial ranking 
-        //Allows for the rare exception where the preset ranking = respondent's answer
-        let initialRanking = {};
-        for(let i=0; i<responseKeys.length; i++){
-            initialRanking[`rank_${i}`] = responseKeys[i]
-        };
-        //Sets answersForSubmit to contain the initial ranking
-        props.handleResponse(questionData.questionId, initialRanking, "rankOrder");
 
-        setItems([ ...dragNDropData ])
-        */
-    }, []);
-
-    const recordSourceName = (name) => {
-        setSourceName(name);
+    const handleChangeValue = (event, receivedValue) => {
+        console.log(`New value received: ${receivedValue}`)
+        //setNewValue(  );
+        newValue = receivedValue
     }
-    const handleChange = (name) => { //"event" tends to bug out to "undefined" 
-        console.log(`Rating change received from ${name}`);
-        
-        const ratingRecorded = document.getElementsByName(name)[0].value
-        console.log(`New rating: ${ratingRecorded}`);
-        
-        let newRatingsToAnswers = rating;
-        newRatingsToAnswers[name] = ratingRecorded;
-        
-        setRating(rating => ({
-            ...rating,
-            [name]: ratingRecorded
+    const handleChangeCommit = (name) => {
+        console.log(`Request made to commit new value for ${name}`)
+        console.log(`Value to commit: ${newValue}`)
+
+        setRecordedValues(value => ({
+            ...value,
+            [name]: newValue
         }));
 
+        //Must do this instead of get from hook state. Otherwise,
+        //value passed to handleResponse is the one before update by setRecordedValues
+        const recordedValuesCopy = recordedValues;
+        recordedValuesCopy[name] = newValue
         //Sets answersForSubmit to new ranking
-        props.handleResponse(props.data.questionId, newRatingsToAnswers, "rateScale");
+        props.handleResponse(props.data.questionId, recordedValuesCopy, "rankOrder");
     }
-    
+
     // Normally you would want to split things out into separate components.
     // But in this example everything is just done in one place for simplicity
     return (
         <React.Fragment>
             {Array(responseKeys.length).fill().map(function(item, i) {
                 return(
-                    <div className={classes.root} key={`${questionData.questionId}_${responseKeys[i]}`} onMouseDown={() => {recordSourceName(responseKeys[i])}}>
+                    <div className={classes.root} key={`${questionData.questionId}_${responseKeys[i]}`}>
                         <Typography id="discrete-slider-always" gutterBottom>
                             {responseValues[i]}
                         </Typography>
-                        <input 
-                            type="range" name={responseKeys[i]} min="1" max="5"
-                            value={rating[responseKeys[i]]}
-                            onChange={() => {handleChange(responseKeys[i])}}
-                        ></input>
+                        <Slider
+                            getAriaValueText={valuetext}
+                            aria-labelledby="discrete-slider-always"
+                            step={1}
+                            valueLabelDisplay="auto" //The bubble that pops up to show the current value on hover
+                            marks={marks}
+                            min={1} //If min-max not set, default is 1-100
+                            max={5}
+                            classes ={{thumb: classes.thumb, mark: classes.mark}}
+                            onChange={handleChangeValue}
+                            onChangeCommitted={changeEvent => {
+                                //This mod removes default "event" and "value" returned
+                                //to instead return the name of this slider
+                                //The only defect is the slider will only respond onMouseUp
+                                handleChangeCommit(responseKeys[i]);
+                            }}
+                            value={recordedValues[responseKeys[i]]}
+                            name={responseKeys[i]}
+                        />
                     </div>
             )
             })}
            
-            <button onClick={() => {console.log(rating)}}>Chk Ratings stored object</button>
+            <button onClick={() => {console.log(recordedValues)}}>Chk value store object</button>
         </React.Fragment>
     );
 }
